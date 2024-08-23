3a0c33ebe92ec02a41c202ce22cf07c5b2f5ec98
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2024-08-23 18:46:08 +0700

    fixing metadata

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+var upload = multer();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,24 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req,res) {
 
+  const file = req.file;
+
+
+  if (!file) {
+    return res.status(400).json({ error: 'No file uploaded'})
+  }
+
+  const {originalname, mimetype, size } = file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+
+});
 
 
 const port = process.env.PORT || 3000;
